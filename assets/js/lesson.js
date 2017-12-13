14aacd9048114550d392ea69405c818cff44ac7e
James Carson <jcarson@tacc.utexas.edu>
2017-12-13 17:10:15 -0600

    Update lesson.js

--- assets/js/lesson.js
+++ assets/js/lesson.js
@@ -3,14 +3,14 @@ $(".solution").click(function(event) {
     var trigger = $(event.target).has(".fold-unfold").size() > 0 ||
         $(event.target).filter(".fold-unfold").size() > 0;
     if (trigger) {
-        $(">*:not(h2)", this).toggle(400);
+        $(">*:not(h3)", this).toggle(400);
         $(">h2>span.fold-unfold", this).toggleClass("fa-caret-square-o-down fa-caret-square-o-up");
         event.stopPropagation();
     }
 });
 
 $(".solution").each(function() {
-    $(">*:not(h2)", this).toggle();
-    var h2 = $("h2:first", this);
-    h2.append("<span class='fold-unfold spacer fa fa-caret-square-o-down'></span>");
+    $(">*:not(h3)", this).toggle();
+    var h3 = $("h3:first", this);
+    h3.append("<span class='fold-unfold spacer fa fa-caret-square-o-down'></span>");
 });
